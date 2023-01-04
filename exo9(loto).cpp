9d13eeb24a3259c6d81fd7d255a574b294594b43
zozowolf <115718673+zozowolf@users.noreply.github.com>
2023-01-04 21:40:10 +0100

    Add files via upload

--- exo9(loto).cpp
+++ exo9(loto).cpp
@@ -10,11 +10,24 @@
 #include <iostream> // entete de gestion des E/S
 #include <windows.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std; // utilisation de l'espace de nommage
                      // de la bibliothéque standard
 void loto(int *tab);
 void affichage(int *tableau, int nombreDeCases);
+
+int aleatoire(int min,int max)
+{
+    int val;
+    
+    do
+    {
+        val =rand();
+    }while( val > max || val < min);
+
+    return val;
+}
 /*======================================================
 Fonction principal
 ========================================================*/
@@ -22,8 +35,32 @@ int main()
 {
     SetConsoleOutputCP(1252);
     // code en dessous
-    int tab[5];
-    loto(tab);
+    int tab[6];
+    bool trouver;
+    int alea,i,j;
+
+    //generateur de nombre aléatoires
+    srand(time(NULL)); // la serie aléatoire va changer toutes les secondes
+
+
+    for ( i = 0; i < 6; i++)
+    {
+        do
+        {
+            alea = aleatoire(1,49);
+            trouver = false;
+            for ( j = 0; j < i - 1; j++)
+            {
+                if (tab[j]==alea)
+                {
+                    trouver=true;
+                }
+            }
+        } while (trouver == true);
+        tab[i] = alea;
+    }
+
+  
     affichage(tab, 6);
 
     // Attendons qu'on appui sur une touche pour terminer
@@ -33,17 +70,7 @@ int main()
     return EXIT_SUCCESS;
 }
 
-void loto(int *tab)
-{
 
-    for (int i = 0; i < 6; i++)
-    {
-        do
-        {
-            tab[i] = rand() % 49 + 1;
-        } while (tab[i] == tab[i - 1, i - 2, i - 3, i - 4, i - 5]);
-    }
-}
 
 void affichage(int *tableau, int nombreDeCases)
 {
